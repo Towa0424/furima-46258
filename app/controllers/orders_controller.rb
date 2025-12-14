59d6c2c11a7989f14f7fe0765427f666c9869936
Towa0424 <w8d42bed72xbyx@gmail.com>
2025-12-14 17:20:54 +0900

    商品購入機能の実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,39 @@
+class OrdersController < ApplicationController
+
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      pay_item
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_address).permit(:postal_code,:prefecture_id,:city,:address_line,:building,:phone,:token).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+   def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: (@item.price * 1.1).floor,
+      card: order_params[:token],
+      currency: 'jpy'
+    ) 
+   end
+end
---
+++ app/models/order_address.rb
@@ -0,0 +1,27 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id,
+                :postal_code, :prefecture_id, :city, :address_line, :building, :phone, :token
+
+  with_options presence: true do
+    validates :token
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/} 
+    validates :prefecture_id
+    validates :city
+    validates :address_line    
+    validates :phone, format: { with: /\A\d{10,11}\z/}
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(
+      order_id: order.id,
+      postal_code: postal_code,
+      prefecture_id: prefecture_id,
+      city: city,
+      address_line: address_line,
+      building: building,
+      phone: phone
+    )
+  end
+end
