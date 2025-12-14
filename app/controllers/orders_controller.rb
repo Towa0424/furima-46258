9bdfd6ddc68b4e753f720962fe798f16f8719a1a
Towa0424 <w8d42bed72xbyx@gmail.com>
2025-12-14 18:23:42 +0900

    購入機能の編集1

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,8 @@
 class OrdersController < ApplicationController
 
+  before_action :authenticate_user!, only: [:index, :create]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
@@ -36,4 +38,11 @@ class OrdersController < ApplicationController
       currency: 'jpy'
     ) 
    end
+
+   def move_to_index
+    if @item.sold_out? || current_user = @item.user
+      redirect_to root_path 
+    end
+   end
+
 end
