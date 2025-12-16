da78a44d4511fadb73209768a9bda49d88304c12
Towa0424 <w8d42bed72xbyx@gmail.com>
2025-12-16 21:58:29 +0900

    gonの位置修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,19 +30,19 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:postal_code,:prefecture_id,:city,:address_line,:building,:phone,:token).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
-   def pay_item
+  def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: order_params[:token],
       currency: 'jpy'
     ) 
-   end
+  end
 
-   def move_to_index
-    if @item.sold_out? || current_user == @item.user
-      redirect_to root_path 
-    end
-   end
+  def move_to_index
+  if @item.sold_out? || current_user == @item.user
+    redirect_to root_path 
+  end
+  end
 
 end
